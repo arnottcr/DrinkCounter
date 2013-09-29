9362c2d9ae05fc738734821404dafc3a92d46dcc
David Spanos <david.spanos404@gmail.com>
2013-09-28 23:41:54 -0400

    full proj commit

--- src/com/example/drinkcount/DrinkCounter.java
+++ src/com/example/drinkcount/DrinkCounter.java
@@ -1,22 +1,79 @@
 package com.example.drinkcount;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
+import java.util.Vector;
 
 public class DrinkCounter extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_drink_counter);
-	}
+TextView text;
+Vector<String> drinkList;
+int counter=0;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.drink_counter, menu);
-		return true;
-	}
+private SensorManager mSensorManager;
+private float mAccel; // acceleration apart from gravity
+private float mAccelCurrent; // current acceleration including gravity
+private float mAccelLast; // last acceleration including gravity
+private long newTime=0;
+private long oldTime=0;
 
+
+ @Override
+ public void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  drinkList = new Vector<String>();
+  setContentView(R.layout.activity_drink_counter);
+  
+  mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+  mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+  mAccel = 0.00f;
+  mAccelCurrent = SensorManager.GRAVITY_EARTH;
+  mAccelLast = SensorManager.GRAVITY_EARTH;
+  
+  text = (TextView) findViewById(R.id.text);
+ }
+  
+  private final SensorEventListener mSensorListener = new SensorEventListener() {
+
+    public void onSensorChanged(SensorEvent se) {
+      float x = se.values[0];
+      float y = se.values[1];
+      float z = se.values[2];
+      mAccelLast = mAccelCurrent;
+      mAccelCurrent = (float) Math.sqrt((double) (x*x + y*y + z*z));
+      float delta = mAccelCurrent - mAccelLast;
+      mAccel = mAccel * 0.9f + delta; // perform low-cut filter
+      
+      newTime=System.currentTimeMillis();
+      if (mAccel>3&&(newTime>(oldTime+1500))){
+    	  drinkList.add("This will be a timestamp");
+    	  text.setText(String.valueOf(drinkList.size()));
+    	  oldTime=System.currentTimeMillis();
+      }
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+  };
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+  }
+
+  @Override
+  protected void onPause() {
+    mSensorManager.unregisterListener(mSensorListener);
+    super.onPause();
+  }
+  
+//  protected void on
+  
 }
